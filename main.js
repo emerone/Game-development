0d3f53704fd55550240777c220fb9f8ec1e10183
Emmanuel <emmanuel.lebaramo@gmail.com>
2023-01-07 21:49:01 +0100

    Setup

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <canvas></canvas>
+`
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const canvasWidth = canvas.width
+const canvasHeight = canvas.height
+
+const playerImage = new Image()
+playerImage = 'shadow_dog.png'
