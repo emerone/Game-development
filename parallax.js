316e32a8337d859c9962f74942aadd03e15fda16
Emmanuel <emmanuel.lebaramo@gmail.com>
2023-01-08 21:57:21 +0100

    Set up for Parrallax.js

---
+++ parallax.js
@@ -0,0 +1,40 @@
+import "./parallax.css";
+import './animations.css'
+import shadowDog from './shadow_dog.png';
+
+document.querySelector('#app').innerHTML = `
+  <canvas></canvas>
+`
+/**
+ * Variables 
+ */ 
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext("2d")
+const canvasWidth = canvas.width = 800
+const canvasHeight = canvas.height = 700
+const gameSpeed = 5
+
+/**
+ * Functions
+ */ 
+
+
+/**
+ * Functions
+ */ 
+
+
+function animate(){
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+  
+
+  requestAnimationFrame(animate)
+}
+animate() 
+
+
+/**
+ * Events 
+ */ 
+
