0b686227cbc86e40ae6cfa5a69655a04c157ecc3
Emmanuel <emmanuel.lebaramo@gmail.com>
2023-01-10 23:44:48 +0100

    Import and show images

--- parallax.js
+++ parallax.js
@@ -18,16 +18,34 @@ const gameSpeed = 5
  * Functions
  */ 
 
+const layer1 = new Image()
+layer1.src = "layer-1.png"
+const layer2 = new Image()
+layer2.src = "layer-2.png"
+const layer3 = new Image()
+layer3.src = "layer-3.png"
+const layer4 = new Image()
+layer4.src = "layer-4.png"
+const layer5 = new Image()
+layer5.src = "layer-5.png"
+
+
 
 /**
  * Functions
  */ 
 
 
+
+
 function animate(){
   ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+  ctx.drawImage(layer1, 0, 0)
+  ctx.drawImage(layer2, 0, 0)
+  ctx.drawImage(layer3, 0, 0)
+  ctx.drawImage(layer4, 0, 0)
+  ctx.drawImage(layer5, 0, 0)
   
-
   requestAnimationFrame(animate)
 }
 animate() 
